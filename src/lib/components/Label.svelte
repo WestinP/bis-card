be75b534fabac4005c9a1a5dda71dd13a4786834
Jack Morris <jack@jackmorris.me>
2024-03-26 23:32:46 -0400

    Add Label component and update Scene component

---
+++ src/lib/components/Label.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Text } from '@threlte/extras';
+	import type { ColorRepresentation } from 'three';
+	import { useReflow } from '@threlte/flex';
+	import { forwardEventHandlers } from '@threlte/core';
+	export let text: string;
+	export let color: ColorRepresentation = 'white';
+	export let z = 0;
+	export let fontStyle:
+		| 'black'
+		| 'bold'
+		| 'extra-bold'
+		| 'extra-light'
+		| 'light'
+		| 'medium'
+		| 'regular'
+		| 'semi-bold'
+		| 'thin' = 'regular';
+	export let anchorX = '50%';
+	export let anchorY = '50%';
+	export let fontSize: number = 1;
+	
+    // $: fontUrl = `/fonts/inter/inter-${fontStyle}.ttf`;
+	
+    const reflow = useReflow();
+</script>
+
+<!-- font={fontUrl} -->
+<Text
+	position.z={z}
+	{text}
+	{anchorX}
+	{anchorY}
+	fontSize={fontSize}
+	{color}
+	on:sync={reflow}
+/>
--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,55 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+	import { T } from '@threlte/core';
+	import { ContactShadows, Float, Grid, OrbitControls, interactivity } from '@threlte/extras';
+	import { Box, Flex, tailwindParser } from '@threlte/flex';
+	import { spring } from 'svelte/motion';
+	import { writable } from 'svelte/store';
+	import { MeshStandardMaterial, PerspectiveCamera } from 'three';
+	import Label from './Label.svelte';
+
+	interactivity();
+
+	let rotX = writable(0);
+	let rotY = writable(0);
+	let rotZ = writable(0);
+
+	let posZ = spring(0, {
+		stiffness: 0.4,
+		damping: 0.4
+	});
+
+	// wiggle effect
+	setInterval(() => {
+		rotX.update((n) => Math.sin(Date.now() / 1000) * 0.1);
+	}, 1000 / 60);
+
+	setInterval(() => {
+		rotY.update((n) => Math.cos(Date.now() / 1000) * 0.1);
+	}, 1000 / 60);
+
+	setInterval(() => {
+		rotZ.update((n) => Math.sin(Date.now() / 1000) * 0.03);
+	}, 1000 / 60);
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
-</T.PerspectiveCamera>
+<T.PerspectiveCamera makeDefault position={[0, 0, 15]} fov={15}></T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
+<Flex>
+	<T.Mesh
+		position.z={$posZ}
+		rotation.x={$rotX}
+		rotation.y={$rotY}
+		rotation.z={$rotZ}
+		on:pointerover={() => posZ.set(1)}
+		on:pointerout={() => posZ.set(0)}
+		on:click={() => {}}
+	>
+		<T.BoxGeometry args={[3.5, 2, 0.025]} />
+		<T.MeshStandardMaterial color="white" metalness={0.8} />
+		<Label text={'please work :3'} z={0.013} fontStyle="semi-bold" fontSize={0.3} color="#FF0000" />
+	</T.Mesh>
+</Flex>
 
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
-/>
+<T.DirectionalLight intensity={4} position.x={5} position.y={10} position.z={15} />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
